9f95cff4ec8e27c6a0b20d4e75af76192de7b2a1
vishnu <vishnu.kv@webnish.com>
2025-03-21 17:52:42 +0530

    Added store

--- src/store.js
+++ src/store.js
@@ -1,14 +1,19 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: ""
 }
 
-//Reducer is not allowed to have any side effects and must not modify existing state. In redux unlike useReducer's reducer function, we pass the initial state as the default state.
-//Before the case used to be in UPPER_CASE. The naming convention is, the domain and then the event (**domain/event**).
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: ""
+}
+
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return {...state, balance: state.balance + action.payload}
@@ -18,16 +23,56 @@ function reducer(state = initialState, action) {
       if (state.loan > 0) return state;
       return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount}
     case "account/payLoan":
-      if (state.loan > 0) return state;
       return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan}
     default:
-      // We used to throe error in the default case. But in redux, it is advised to return the state.
       return state;
   }
 }
 
-const store = createStore(reducer);
-store.dispatch({type: "account/deposit", payload: 500});
-store.dispatch({type: "account/withdraw", payload: 200});
-store.dispatch({type: "account/requestLoan", payload: {amount: 1000, purpose: "Buy a car"}})
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt }
+    case "customer/updateName":
+      return {...state, fullName: action.payload}
+    default:
+      return state
+  }
+}
+
+function deposit(amount) {
+  return {type: "account/deposit", payload: amount}
+}
+
+function withdraw(amount) {
+  return {type: "account/withdraw", payload: amount}
+}
+
+function requestLoan(amount, purpose) {
+  return {type: "account/requestLoan", payload: {amount, purpose}}
+}
+
+function payLoan() {
+  return {type: "account/payLoan"}
+}
+
+function createCustomer(fullName, nationalID) {
+  //calculations could've been done inside the reducer function. But since date calculation is a side effect and the reducer must always be a pure function, we can't have it inside the reducer.
+  return {type: "customer/createCustomer", payload: {fullName, nationalID, createdAt: new Date().toString()}}
+}
+
+function updateName(fullName) {
+  return {type: "customer/updateName", payload: fullName}
+}
+
+const rootReducer = combineReducers({account: accountReducer, customer: customerReducer})
+const store = createStore(rootReducer);
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, "Buy a car"))
+store.dispatch(payLoan())
+
+store.dispatch(createCustomer("John doe", "dd4e75e"))
+console.log(store.getState());
+store.dispatch(updateName("Jane doe"))
 console.log(store.getState());
